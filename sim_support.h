93867c459632961dd081c62f93a67465eb9061e3
Zbigniew Chamski <zbigniew.chamski@bristol.ac.uk>
2019-07-10 14:00:33 +0200

    Add counting of taken branches to non word-aligned destinations.
    * sim_main.c (main): Increment counter of non-word branch destinations if
      branch target is not on 4-byte boundary.
    * sim_support.c (nonword_branch_destinations): New.
      (printStats): Print the count of non word-aligned taken branch
      destinations.
    * sim_support.h (nonword_branch_destinations): New.

--- sim_support.h
+++ sim_support.h
@@ -35,6 +35,7 @@ extern u64 flash_data_reads;
 extern u64 flash_insn_reads;
 extern u64 flash_writes;
 extern u64 taken_branches;
+extern u64 nonword_branch_destinations;
 extern bool takenBranch;    // Informs fetch that previous instruction caused a control flow change
 extern void sim_exit(int);  // All sim ends lead through here
 void cpu_reset();           // Resets the CPU according to the specification
